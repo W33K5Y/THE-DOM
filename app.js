081d41e7590dd73c7b6ecf362bbf8c4ba16d7777
Sam Weeks <weeksy_17@icloud.com>
2021-01-16 13:12:00 +0000

    console message bug fix

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const div = document.getElementsByTagName('DIV')
 const squareIds = rectangleWrap.children;
 
 let showIDs = (func) => {
+    console.log(`The Squares ID's are`)
   for(divs of squareIds) {
-        console.log(`The Square ID's are ${divs.firstElementChild.id}`)
+        console.log(` ${divs.firstElementChild.id}`)
     }
 }
 
